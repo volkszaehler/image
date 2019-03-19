2d1a4590ddff7ba731fe3acd74ea56f2e29afccb
andig <cpuidle@gmx.de>
2019-03-19 12:54:40 +0100

    Add logrotate for vzlogger

--- volkszaehler/04-vzlogger/01-run.sh
+++ volkszaehler/04-vzlogger/01-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 install -m 644 files/vzlogger.service "${ROOTFS_DIR}/etc/systemd/system/vzlogger.service"
+install -m 644 files/vzlogger "${ROOTFS_DIR}/etc/logrotate.d/vzlogger"
 
 on_chroot << EOF
 if [ -d /home/pi/vzlogger ]; then
