4c9f55f6d8b428aa55cc302962825a8af420eed0
andig <cpuidle@gmx.de>
2019-03-19 12:54:26 +0100

    Move composer install first

--- volkszaehler/03-volkszaehler/01-run.sh
+++ volkszaehler/03-volkszaehler/01-run.sh
@@ -10,6 +10,16 @@ apt install -y -t buster php-fpm php-gd php-intl php-mbstring php-mysql php-imap
 EOF
 
 on_chroot << EOF
+if [ ! -e /usr/local/bin/composer ]; then
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    # php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    php composer-setup.php
+    php -r "unlink('composer-setup.php');"
+    mv composer.phar /usr/local/bin/composer
+fi
+EOF
+
+on_chroot << EOF
 if [ -d /home/pi/volkszaehler ]; then
     cd /home/pi/volkszaehler
     git checkout master
@@ -22,14 +32,7 @@ fi
 # version 1.0
 git reset --hard 4a0bbedf9d097c038f602df36a83f8df7ee5e9ec
 
-if [ ! -e /usr/local/bin/composer ]; then
-    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    # php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-    php composer-setup.php
-    php -r "unlink('composer-setup.php');"
-    mv composer.phar /usr/local/bin/composer
-fi
-
+# composer update
 if [ ! -e /home/pi/volkszaehler/composer.lock ]; then
     composer update
 fi
