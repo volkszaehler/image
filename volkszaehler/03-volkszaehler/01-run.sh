99a1d6ff54009589890a57934bf95e5c22ae68b1
frankrichter <frank.richter83@gmail.com>
2019-04-12 16:50:03 +0200

    Switch to Raspbian Buster (#3)

--- volkszaehler/03-volkszaehler/01-run.sh
+++ volkszaehler/03-volkszaehler/01-run.sh
@@ -1,13 +1,6 @@
 #!/bin/bash -e
 
-install -m 644 files/10-buster.list		"${ROOTFS_DIR}/etc/apt/sources.list.d/"
-install -m 644 files/10-buster		"${ROOTFS_DIR}/etc/apt/preferences.d/"
 install -m 644 files/middleware.service "${ROOTFS_DIR}/etc/systemd/system/middleware.service"
-
-on_chroot << EOF
-apt update
-apt install -y -t buster php-fpm php-gd php-intl php-mbstring php-mysql php-imap php7.3-opcache php-sqlite3 php-xml php-xmlrpc php-zip php-cli php-apcu
-EOF
 
 on_chroot << EOF
 if [ ! -e /usr/local/bin/composer ]; then
@@ -29,11 +22,12 @@ else
     cd /home/pi/volkszaehler
 fi
 
-# version 1.0
-git reset --hard 4a0bbedf9d097c038f602df36a83f8df7ee5e9ec
-
 # composer update
 if [ ! -e /home/pi/volkszaehler/composer.lock ]; then
-    composer update
+    composer update --no-dev
 fi
+
+cp etc/config.dist.yaml etc/config.yaml
+chown -R pi:pi /home/pi/volkszaehler
+
 EOF
